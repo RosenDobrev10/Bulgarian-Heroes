381bd80af09a80df3bd8b0b514abd2163a0acaa7
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-10 04:44:37 +0200

    change place of the assets folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
-	return <></>;
+	const [heroes, setHeroes] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:3030/data/heroes')
+			.then((res) => res.json())
+			.then((heroes) => {
+				console.log(heroes);
+				setHeroes(heroes);
+			});
+	}, []);
+
+	return (
+		<>
+			{heroes.map((h) => (
+				<>
+					<h1 key={h._id}>{h.name}</h1>
+					<img src={h.imageUrl} />
+				</>
+			))}
+		</>
+	);
 }
