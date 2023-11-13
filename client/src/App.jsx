9eb0b37c9ed4ece2ecf89b86b45b836dcb3bd08e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-13 15:15:09 +0200

    Uncomment Login Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Footer from './components/Footer/Footer.jsx';
 import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
 import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
 import NotFound from './components/NotFound/NotFound.jsx';
 
 export default function App() {
@@ -16,7 +18,7 @@ export default function App() {
 				{/* <Route path="/search" element={<Search />} /> */}
 				{/* <Route path="/add" element={<Add />} /> */}
 				{/* <Route path="/profile" element={<Profile />} /> */}
-				{/* <Route path="/login" element={<Login />} /> */}
+				<Route path="/login" element={<Login />} />
 				{/* <Route path="/register" element={<Register />} /> */}
 				{/* <Route path="/logout" element={<Logout />} /> */}
 				<Route path="*" element={<NotFound />} />
