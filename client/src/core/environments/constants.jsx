b8ac9230025df20d0f24d7519b79f73aad6f81d6
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-14 14:46:12 +0200

    Update constants.jsx

--- client/src/core/environments/constants.jsx
+++ client/src/core/environments/constants.jsx
@@ -4,20 +4,16 @@ export const endpoints = {
 	register: 'users/register',
 	login: 'users/login',
 	logout: 'users/logout',
-	catalog: 'data/heroes?distinct=name&sortBy=born',
+	getAllHeroes: 'data/heroes?distinct=name&sortBy=_createdOn%20desc',
 	createHero: 'data/heroes',
 	likeHero: 'data/likes',
-	details: (heroId) => `data/heroes/${heroId}`,
-	edit: (heroId) => `data/heroes/${heroId}`,
-	delete: (heroId) => `data/heroes/${heroId}`,
-	canLike: (heroId, userId) =>
-		`data/likes?where=heroId%3D%22${heroId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
-	likesForPlayer: (heroId) =>
-		`data/likes?where=heroId%3D%22${heroId}%22&distinct=_ownerId&count`,
-	search: (query) =>
-		`data/heroes?where=name%20LIKE%20%22${query}%22&sortBy=born`,
-	getMyHeroes: (userId) =>
-		`data/heroes?where=_ownerId%3D%22${userId}%22&sortBy=born`
+	getHeroById: (heroId) => `data/heroes/${heroId}`,
+	editHero: (heroId) => `data/heroes/${heroId}`,
+	deleteHero: (heroId) => `data/heroes/${heroId}`,
+	canLike: (heroId, userId) => `data/likes?where=heroId%3D%22${heroId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
+	likesForHero: (heroId) => `data/likes?where=heroId%3D%22${heroId}%22&distinct=_ownerId&count`,
+	search: (query) => `data/heroes?where=name%20LIKE%20%22${query}%22&sortBy=_createdOn%20desc`,
+	getMyHeroes: (userId) => `data/heroes?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`
 };
 
 export const tokenName = '719c660ebedf977ef42a8b11181c52688a3c163d823dceae56e027ef79902615'
