27b6edcf7f9da411deee4b4752312c7953f92518
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-14 14:45:50 +0200

    Create heroesApi.jsx

---
+++ client/src/core/api/heroesApi.jsx
@@ -0,0 +1,34 @@
+import * as api from './api.jsx';
+import { endpoints } from '../environments/constants.jsx';
+
+export async function getAllHeroes() {
+	return api.get(endpoints.getAllHeroes);
+}
+
+export async function createHero(heroData) {
+	return api.post(endpoints.createHero, heroData);
+}
+
+export async function getHeroById(heroId) {
+	return api.get(endpoints.details(heroId));
+}
+
+export async function deleteHeroById(heroId) {
+	return api.del(endpoints.deleteHero(heroId));
+}
+
+export async function editHeroById(heroId, heroData) {
+	return api.put(endpoints.editHero(heroId), heroData);
+}
+
+export async function likeHero(heroId) {
+	return api.post(endpoints.likeHero(heroId));
+}
+
+export async function likesForHero(heroId) {
+	return api.get(endpoints.likesForHero(heroId));
+}
+
+export async function canLike(heroId, userId) {
+	return api.get(endpoints.canLike(heroId, userId));
+}
