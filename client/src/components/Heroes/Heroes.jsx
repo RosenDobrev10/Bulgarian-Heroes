3c9a5fe53f71ef839366f66d1e4854ad3856fc07
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 18:28:27 +0200

    Update Heroes.jsx

--- client/src/components/Heroes/Heroes.jsx
+++ client/src/components/Heroes/Heroes.jsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Spinner from '../Spinner/Spinner.jsx';
+import { getAllHeroes } from '../../core/api/heroesApi.js';
 
 export default function Heroes() {
+	const [heroes, setHeroes] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
 	useEffect(() => {
 		document.title = 'Герои';
+		getAllHeroes()
+			.then((heroes) => setHeroes(heroes))
+			.catch((err) => console.error(err))
+			.finally(() => setIsLoading(false));
 	}, []);
 
-	return <Spinner />;
+	return (
+		<>
+			{isLoading && <Spinner />}
+			{heroes}
+		</>
+	);
 }
