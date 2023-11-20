08dc2aad022bd43019e78fc3f66e036ec79765f9
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 12:22:34 +0200

    Add title to page

--- client/src/components/Heroes/Heroes.jsx
+++ client/src/components/Heroes/Heroes.jsx
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
 import Spinner from '../Spinner/Spinner.jsx';
 
 export default function Heroes() {
+	useEffect(() => {
+		document.title = 'Heroes';
+	}, []);
+
 	return <Spinner />;
 }
