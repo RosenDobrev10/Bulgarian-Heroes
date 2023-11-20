3b6cdefeb42f97d2e3ea0a2c456f0f96180305b6
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 15:35:57 +0200

    Create AuthContext.jsx

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { createContext } from 'react';
+import { useSessionStorage } from '../hooks/useSessionStorage';
+
+export const AuthContext = createContext(null); // Create AuthContext
+
+export const AuthProvider = ({ children }) => {
+	// Create auth provider (to use in app only use AuthProvider around the routes)
+	const sessionManager = useSessionStorage();
+
+	const contextValues = {
+		addUserSession: (userData) => sessionManager.setUserState(userData),
+		clearUserSession: () => sessionManager.clearUserState(),
+		getUserToken: sessionManager.currentUserToken,
+		getUserDetails: sessionManager.currentUserData,
+		isLoggedIn: !!sessionManager.currentUserToken
+	};
+
+	return (
+		<AuthContext.Provider value={contextValues}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
