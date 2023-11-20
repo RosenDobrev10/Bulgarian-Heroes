32bea0f053537282d8901bdd0a34a38afe1dfaef
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 12:18:43 +0200

    Add title to page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
 import CarouselComp from './Carousel/CarouselComp.jsx';
 
 export default function Home() {
+	useEffect(() => {
+		document.title = 'Home';
+	}, []);
+
 	return <CarouselComp />;
 }
