b5ab21403ad69e223871dda8879b88797304a41e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 16:14:59 +0200

    Better comments

--- client/src/hooks/useSessionStorage.js
+++ client/src/hooks/useSessionStorage.js
@@ -5,7 +5,7 @@ export const useSessionStorage = () => {
 	// User data is saved in context to easy use in app: { _id, username, email }
 	const [currentUserData, setCurrentUserData] = useState({});
 
-	// Initial user token is saved in local storage --> can be null or accessToken
+	// Initial user token is saved in local storage -> can be null or accessToken
 	const [currentUserToken, setCurrentUserToken] = useState(() => {
 		try {
 			// Verify that the data in the local storage is in the correct format
@@ -21,21 +21,19 @@ export const useSessionStorage = () => {
 		return null;
 	});
 
-	// Set new user state
 	const setUserState = (userData) => {
 		// If has a userData set new user state and user data
 		if (userData) {
-			localStorage.setItem(tokenName,JSON.stringify(userData.accessToken));
-			setCurrentUserToken(userData.accessToken); // The token is used to make a request to the server
+			localStorage.setItem(tokenName, JSON.stringify(userData.accessToken));
+			setCurrentUserToken(userData.accessToken); // Token is used to make requests to the server
 			setCurrentUserData(userData.userDetails); // User data is used to get needed properties in entire app using a context
 		}
 	};
 
-	// Clear current user state
 	const clearUserState = () => {
 		localStorage.removeItem(tokenName);
-		setCurrentUserToken(null); // Set new state to null
-		setCurrentUserData({}); // Set new state to empty object
+		setCurrentUserToken(null); 
+		setCurrentUserData({});
 	};
 
 	return {
