073e7be76ec38248c07fb890f22f3df2cb7de021
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-20 16:12:35 +0200

    Add extension to import

--- client/src/hooks/useAuthContext.js
+++ client/src/hooks/useAuthContext.js
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
-
-import { AuthContext } from '../contexts/AuthContext';
+import { AuthContext } from '../contexts/AuthContext.jsx';
 
 export const useAuthContext = () => {
 	const context = useContext(AuthContext);
