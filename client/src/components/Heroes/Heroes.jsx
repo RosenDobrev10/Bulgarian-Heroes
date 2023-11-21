aade77dfef48e71f811ef44eff286f12a44fe351
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-21 16:39:09 +0200

    Update Heroes.jsx

--- client/src/components/Heroes/Heroes.jsx
+++ client/src/components/Heroes/Heroes.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import Spinner from '../Spinner/Spinner.jsx';
 import { getAllHeroes } from '../../core/api/heroesApi.js';
+import Spinner from '../Spinner/Spinner.jsx';
+import Hero from './Hero/Hero.jsx';
 
 export default function Heroes() {
 	const [heroes, setHeroes] = useState([]);
@@ -9,7 +10,7 @@ export default function Heroes() {
 	useEffect(() => {
 		document.title = 'Герои';
 		getAllHeroes()
-			.then((heroes) => setHeroes(heroes))
+			.then((data) => setHeroes(data))
 			.catch((err) => console.error(err))
 			.finally(() => setIsLoading(false));
 	}, []);
@@ -17,7 +18,26 @@ export default function Heroes() {
 	return (
 		<>
 			{isLoading && <Spinner />}
-			{heroes}
+			<section className="text-neutral-700 dark:text-neutral-300">
+				<div className="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+					<h3 className="mb-6 text-3xl font-bold">Testimonials</h3>
+					<p className="mb-6 pb-2 md:mb-12 md:pb-0">
+						Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit. Fugit, error amet numquam iure provident voluptate
+						esse quasi, veritatis totam voluptas nostrum quisquam
+						eum porro a pariatur veniam.
+					</p>
+				</div>
+				{heroes.map((hero) => (
+					<Hero key={hero._id} {...hero} />
+				))}
+				{/*First Testimonial*/}
+				<div className="grid gap-6 text-center md:grid-cols-3"></div>
+			</section>
 		</>
+
+		// <div className='flex justify-around'>
+		// 	{heroes.map((hero) => <Hero key={hero._id} {...hero} /> )}
+		// </div>
 	);
 }
