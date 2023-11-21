a023526676d32b57fbf86e9fce2fc265d881af0f
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-21 18:32:19 +0200

    Update Heroes.jsx

--- client/src/components/Heroes/Heroes.jsx
+++ client/src/components/Heroes/Heroes.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
+
 import { getAllHeroes } from '../../core/api/heroesApi.js';
+
 import Spinner from '../Spinner/Spinner.jsx';
 import Hero from './Hero/Hero.jsx';
+
+import { Animate, initTE } from 'tw-elements';
+
+initTE({ Animate });
 
 export default function Heroes() {
 	const [heroes, setHeroes] = useState([]);
@@ -18,26 +24,13 @@ export default function Heroes() {
 	return (
 		<>
 			{isLoading && <Spinner />}
-			<section className="text-neutral-700 dark:text-neutral-300">
-				<div className="mx-auto text-center md:max-w-xl lg:max-w-3xl">
-					<h3 className="mb-6 text-3xl font-bold">Testimonials</h3>
-					<p className="mb-6 pb-2 md:mb-12 md:pb-0">
-						Lorem ipsum dolor sit amet, consectetur adipisicing
-						elit. Fugit, error amet numquam iure provident voluptate
-						esse quasi, veritatis totam voluptas nostrum quisquam
-						eum porro a pariatur veniam.
-					</p>
-				</div>
+			<ul className="flex flex-wrap justify-around my-3">
 				{heroes.map((hero) => (
-					<Hero key={hero._id} {...hero} />
+					<li key={hero._id} className="w-1/3 h-auto">
+						<Hero {...hero} />
+					</li>
 				))}
-				{/*First Testimonial*/}
-				<div className="grid gap-6 text-center md:grid-cols-3"></div>
-			</section>
+			</ul>
 		</>
-
-		// <div className='flex justify-around'>
-		// 	{heroes.map((hero) => <Hero key={hero._id} {...hero} /> )}
-		// </div>
 	);
 }
