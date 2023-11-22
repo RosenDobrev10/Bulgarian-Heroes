1fb32e6cff09ba59b0e921068421eeaa7205949a
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-22 11:24:24 +0200

    Remove when hero is added

--- client/src/components/Heroes/Hero/Hero.jsx
+++ client/src/components/Heroes/Hero/Hero.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 import { Ripple, initTE } from 'tw-elements';
 initTE({ Ripple });
 
-import formatDateToTimeAgo from '../../../util/formatDateToTimeAgo.js';
-
-export default function Hero({ _id, name, imageUrl, description, _createdOn }) {
+export default function Hero({ _id, name, imageUrl, description, }) {
 	return (
 		<>
 			<div className="block rounded-lg bg-green-500 shadow-2xl">
@@ -29,18 +27,17 @@ export default function Hero({ _id, name, imageUrl, description, _createdOn }) {
 						{name}
 					</h5>
 					<p className="mb-4 text-base text-white">{description}</p>
-					<p className="mb-4 text-base text-white">
-						Добавен преди {formatDateToTimeAgo(_createdOn)}.
-					</p>
-					<span className="mr-20 mb-4 text-base text-white">
-						Харесвания: 
-					</span>
-					<Link
-						to={`/heroes/${_id}`}
-						className="px-3 py-2 bg-red-500 text-white text-xs font-bold uppercase rounded"
-					>
-						Научи още ...
-					</Link>
+					<div className='flex justify-between'>
+						<p className="mb-4 text-base text-white">
+							Харесвания:
+						</p>
+						<Link
+							to={`/heroes/${_id}`}
+							className="px-6 py-2 bg-red-500 text-white text-base font-bold uppercase rounded"
+						>
+							Научи още ...
+						</Link>
+					</div>
 				</div>
 			</div>
 		</>
