b36b4eae68db4367a6d1fa92bf0565e4b05c93a2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-22 15:24:38 +0200

    use correct path for api

--- client/src/core/api/userApi.js
+++ client/src/core/api/userApi.js
@@ -1,22 +1,16 @@
-import * as api from './api.js/index.js'
+import * as api from '../api/api.js'
 import { endpoints } from '../environments/constants.js';
-
-import { clearUserData, setUserData } from '../../util/userSession.jsx';
-
 
 export async function login(email, password) {
 	const user = await api.post(endpoints.login, { email, password });
-	setUserData(user);
 	return user;
 }
 
 export async function register(email, password) {
 	const user = await api.post(endpoints.register, { email, password });
-	setUserData(user);
 	return user;
 }
 
 export async function logout() {
 	api.get(endpoints.logout);
-	clearUserData();
 }
