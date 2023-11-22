fec9ee7f61cd95358cbc8f1410fdc3019ed592c6
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-22 17:32:08 +0200

    Add isOwner functionality

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -5,26 +5,30 @@ import { getHeroById } from '../../core/api/heroesApi.js';
 import formatDateToTimeAgo from '../../util/formatDateToTimeAgo.js';
 
 import Spinner from '../Spinner/Spinner.jsx';
+import { useAuthContext } from '../../hooks/useAuthContext.js';
 
 export default function Details() {
 	const [hero, setHero] = useState({});
+	const [isOwner, setIsOwner] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	const { heroId } = useParams();
+	const { getUserId } = useAuthContext();
 
 	useEffect(() => {
 		getHeroById(heroId)
 			.then((data) => {
 				setHero(data);
+				setIsOwner(getUserId === data._ownerId);
 				document.title = data.name;
 			})
 			.catch((err) => console.error(err))
 			.finally(() => setIsLoading(false));
-	}, [heroId]);
+	}, [heroId, getUserId]);
 
 	return (
 		<>
 			{isLoading && <Spinner />}
-			<div className="flex flex-col rounded-lg mx-4 bg-green-500 shadow-2xl dark:bg-neutral-700 md:flex-row">
+			<div className="flex flex-col rounded-lg m-10 bg-green-500 shadow-2xl dark:bg-neutral-700 md:flex-row">
 				<img
 					className="aspect-video h-screen w-2/3 object-fill rounded-t-lg md:h-auto md:!rounded-none md:!rounded-l-lg"
 					src={hero.imageUrl}
@@ -46,16 +50,28 @@ export default function Details() {
 					<p className="mb-4 text-base text-white ">
 						Добавен преди {formatDateToTimeAgo(hero._createdOn)}.
 					</p>
-					<div className="bg-red-500 w-72 lg:w-5/6 m-auto mt-6 p-2 rounded-2xl  text-white text-center shadow-xl">
-						<Link className="lg:text-sm text-lg font-bold">
-							Изтрий
-						</Link>
-					</div>
-					<div className="bg-yellow-500 w-72 lg:w-5/6 m-auto mt-6 p-2 rounded-2xl text-center text-white shadow-xl">
-						<Link className="lg:text-sm text-lg font-bold">
-							Промени
-						</Link>
-					</div>
+					{isOwner ? (
+						<>
+							<div className="hover:drop-shadow-lg hover:opacity-80 bg-red-500 w-72 lg:w-5/6 m-auto mt-6 p-2 rounded-2xl  text-white text-center shadow-xl">
+								<Link className="lg:text-sm text-lg font-bold">
+									Изтрий
+								</Link>
+							</div>
+							<div className="hover:drop-shadow-lg hover:opacity-80 bg-yellow-500 w-72 lg:w-5/6 m-auto mt-6 p-2 rounded-2xl text-center text-white shadow-xl">
+								<Link className="lg:text-sm text-lg font-bold">
+									Промени
+								</Link>
+							</div>
+						</>
+					) : (
+						<>
+							<div className="hover:drop-shadow-lg hover:opacity-80 bg-blue-500 w-72 lg:w-5/6 m-auto mt-6 p-2 rounded-2xl  text-white text-center shadow-xl">
+								<Link className="lg:text-sm text-lg font-bold">
+									Харесай
+								</Link>
+							</div>
+						</>
+					)}
 				</div>
 			</div>
 		</>
