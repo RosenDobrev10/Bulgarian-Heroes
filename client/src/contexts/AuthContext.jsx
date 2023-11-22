ce622676d0e8d9099ab9e9782c0cc2df7a69ae4e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-22 16:44:16 +0200

    add login, register and logout handlers

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,19 +1,58 @@
 /* eslint-disable react/prop-types */
 import { createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { useSessionStorage } from '../hooks/useSessionStorage.js';
+import { login, logout, register } from '../core/api/userApi.js';
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
 	// create AuthProvider so we can use it everywhere in our app(put AuthProvider around all other components)
 	const sessionManager = useSessionStorage();
+	const navigate = useNavigate();
+
+	const loginSubmitHandler = async (formValues) => {
+		try {
+			const result = await login(formValues.email, formValues.password);
+			sessionManager.setUserState(result);
+			navigate('/heroes');
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	const registerSubmitHandler = async (formValues) => {
+		try {
+			if (formValues.password === formValues.repass) {
+				const result = await register(formValues.email, formValues.password);
+				sessionManager.setUserState(result);
+				navigate('/heroes');
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	const logoutHandler = async () => {
+		try {
+			await logout();
+			sessionManager.clearUserState();
+			navigate('/login');
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	const contextValues = {
 		addUserSession: (userData) => sessionManager.setUserState(userData),
 		clearUserSession: () => sessionManager.clearUserState(),
 		getUserToken: sessionManager.currentUserData?.accessToken,
 		getUserEmail: sessionManager.currentUserData?.email,
-		isLoggedIn: !!sessionManager.currentUserData?.accessToken
+		isLoggedIn: !!sessionManager.currentUserData?.accessToken,
+		loginSubmitHandler,
+		registerSubmitHandler,
+		logoutHandler
 	};
 
 	return (
