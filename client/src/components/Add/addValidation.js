d634880e9c13b7242ec5ee691fe1dfb81de24987
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-23 17:48:52 +0200

    Create addValidation.js

---
+++ client/src/components/Add/addValidation.js
@@ -0,0 +1,47 @@
+import { addFormKeys } from '../../core/environments/constants.js';
+
+export default function addValidation(inputName, inputValue) {
+	if (inputName === addFormKeys.name) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		} else if (inputValue.length < 2) {
+			return { [inputName]: 'Минимум 2 символа !' };
+		}
+		return { [inputName]: '' };
+	} else if (inputName === addFormKeys.imageUrl) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		}
+		return { [inputName]: '' };
+	} else if (inputName === addFormKeys.occupation) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		} else if (inputValue.length < 2) {
+			return { [inputName]: 'Минимум 2 символа !' };
+		}
+		return { [inputName]: '' };
+	} else if (inputName === addFormKeys.birthplace) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		} else if (inputValue.length < 2) {
+			return { [inputName]: 'Минимум 2 символа !' };
+		}
+		return { [inputName]: '' };
+	} else if (inputName === addFormKeys.born) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		} else if (Number(inputValue) > 2023) {
+			return { [inputName]: 'Невалидна година !' };
+		}
+		return { [inputName]: '' };
+	} else if (inputName === addFormKeys.description) {
+		if (inputValue === '') {
+			return { [inputName]: 'Полето е задължително !' };
+		} else if (inputValue.length < 10) {
+			return { [inputName]: 'Минимум 10 символа !' };
+		}  else if (inputValue.length < 500) {
+			return { [inputName]: 'Максимум 500 символа !' };
+		}
+		return { [inputName]: '' };
+	}
+}
