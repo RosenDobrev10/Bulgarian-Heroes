e1ec8dcab34523cd6a5287abcbfa7fedd6604937
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-23 17:49:28 +0200

    Remove else clause

--- client/src/components/Login/loginValidation.js
+++ client/src/components/Login/loginValidation.js
@@ -5,19 +5,17 @@ const pattern = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
 export default function loginValidation(inputName, inputValue) {
 	if (inputName === loginFormKeys.email) {
 		if (inputValue === '') {
-			return { [inputName]: 'Полето е задължително!' };
+			return { [inputName]: 'Полето е задължително !' };
 		} else if (pattern.test(inputValue) === false) {
-			return { [inputName]: 'Невалиден формат!' };
-		} else {
-			return { [inputName]: '' };
+			return { [inputName]: 'Невалиден формат !' };
 		}
+		return { [inputName]: '' };
 	} else if (inputName === loginFormKeys.password) {
 		if (inputValue === '') {
-			return { [inputName]: 'Полето е задължително!' };
+			return { [inputName]: 'Полето е задължително !' };
 		} else if (inputValue.length < 6) {
-			return { [inputName]: 'Паролата е къса!' };
-		} else {
-			return { [inputName]: '' };
+			return { [inputName]: 'Минимум 6 символа !' };
 		}
+		return { [inputName]: '' };
 	}
 }
--- client/src/components/Register/registerValidation.js
+++ client/src/components/Register/registerValidation.js
@@ -5,27 +5,24 @@ const pattern = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
 export default function registerValidation(inputName, inputValue) {
 	if (inputName === registerFormKeys.email) {
 		if (inputValue === '') {
-			return { [inputName]: 'Полето е задължително!' };
+			return { [inputName]: 'Полето е задължително !' };
 		} else if (pattern.test(inputValue) === false) {
-			return { [inputName]: 'Невалиден формат!' };
-		} else {
-			return { [inputName]: '' };
+			return { [inputName]: 'Невалиден формат !' };
 		}
+		return { [inputName]: '' };
 	} else if (inputName === registerFormKeys.password) {
 		if (inputValue === '') {
-			return { [inputName]: 'Полето е задължително!' };
+			return { [inputName]: 'Полето е задължително !' };
 		} else if (inputValue.length < 6) {
-			return { [inputName]: 'Паролата е къса!' };
-		} else {
-			return { [inputName]: '' };
+			return { [inputName]: 'Минимум 6 символа !' };
 		}
+		return { [inputName]: '' };
 	} else if (inputName === registerFormKeys.repass) {
 		if (inputValue === '') {
-			return { [inputName]: 'Полето е задължително!' };
+			return { [inputName]: 'Полето е задължително !' };
 		} else if (inputValue.length < 6) {
-			return { [inputName]: 'Паролата е къса!' };
-		} else {
-			return { [inputName]: '' };
+			return { [inputName]: 'Минимум 6 символа !' };
 		}
+		return { [inputName]: '' };
 	}
 }
