851a8e80a6df27f7a3179bead7fa85557608d664
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-23 17:04:39 +0200

    Add validate function to useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 
-export default function useForm(submitHandler, initialValues) {
+export default function useForm(submitHandler, initialValues, validateFunction) {
 	const [formValues, setFormValues] = useState(initialValues);
-	const [errorMessage, setErrorMessage] = useState('');
-	const [isLoading, setIsLoading] = useState(false)
+	const [serverErrorMessage, setServerErrorMessage] = useState('');
+	const [formErrorMessage, setFormErrorMessage] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 
 	const onChange = (e) => {
 		setFormValues((state) => ({
@@ -12,17 +13,31 @@ export default function useForm(submitHandler, initialValues) {
 		}));
 	};
 
+	const onBlur = (e) => {
+		const currentErrors = validateFunction(e.target.name, e.target.value);
+		console.log(currentErrors);
+		setFormErrorMessage((state) => ({ ...state, ...currentErrors }));
+	};
+
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		try {
 			setIsLoading(true);
 			await submitHandler(formValues);
 		} catch (error) {
-			setErrorMessage(error.message);
+			setServerErrorMessage(error.message);
 		} finally {
 			setIsLoading(false);
 		}
 	};
 
-	return { formValues, errorMessage, isLoading, onChange, onSubmit };
+	return {
+		formValues,
+		formErrorMessage,
+		serverErrorMessage,
+		isLoading,
+		onChange,
+		onBlur,
+		onSubmit
+	};
 }
