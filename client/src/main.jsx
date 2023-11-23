35a8c909ecc29b8a0427a8cbc35b414d188c9cc6
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-11-23 13:39:50 +0200

    Add ErrorBoundary to prevent white screen death

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,17 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
 import './index.css';
+
+import App from './App.jsx';
+import { ErrorBoundary } from './guards/ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
+	<ErrorBoundary>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
+	</ErrorBoundary>
 	// </React.StrictMode>
 );
