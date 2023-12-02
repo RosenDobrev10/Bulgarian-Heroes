a4a63349024feac4782137ca0f362154e1a09270
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-02 13:28:54 +0200

    add initial likes count to 0

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -11,7 +11,7 @@ import Spinner from '../Spinner/Spinner.jsx';
 import Message from '../Message/Message.jsx';
 
 export default function Details() {
-	const [hero, setHero] = useState({});
+	const [hero, setHero] = useState({likes: 0});
 	const [isOwner, setIsOwner] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	const [errorMessage, setErrorMessage] = useState('');
