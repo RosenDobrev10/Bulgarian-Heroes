7d5a23139c7b3334bf3743629a282d02d3451e55
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-03 18:40:51 +0200

    Add NoResults component

---
+++ client/src/components/NoResults/NoResults.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+import styles from './NoResults.module.css';
+
+export default function NoResults({ text, buttonText }) {
+	const characters = text.split('');
+
+	return (
+		<div className={styles.waviy}>
+			{characters.map((char, index) => (
+				<span
+					key={index}
+					style={{ '--i': index + 1 }}
+					className={styles.waviySpan}
+				>
+					{char !== ' ' ? char : '\u00A0' }
+				</span>
+			))}
+            {buttonText && (
+                <Link 
+                to='/add' 
+                className=" bg-green-500 shadow-2xl hover:bg-gradient-to-b ml-5 from-white via-green-500 to-red-500 hover:text-white text-white font-semibold px-10 py-3 rounded-md mr-6"
+                >
+                {buttonText}
+                </Link>
+            )}
+		</div>
+	);
+}
