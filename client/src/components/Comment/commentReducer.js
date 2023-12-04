d9efeced5158ad863744ccb8f54ed2ee27be7b4b
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-05 00:19:41 +0200

    add new comment first in the array of comments

--- client/src/components/Comment/commentReducer.js
+++ client/src/components/Comment/commentReducer.js
@@ -1,8 +1,8 @@
 export default function commentReducer(state, action) {
-	if (action.type === 'GET_ALL_COMMENTS_FOR_HERO') {
+	if (action?.type === 'GET_ALL_COMMENTS_FOR_HERO') {
 		return [...action.payload];
 	} else if (action.type === 'ADD_COMMENT') {
-		return [...state, action.payload];
+		return [action.payload, ...state,];
 	} else if (action.type === 'EDIT_COMMENT') {
 		return state.map((comment) => comment._id === action.payload._id ? { ...comment, text: action.payload.text } : comment);
 	} else if (action.type === 'DELETE_COMMENT') {
