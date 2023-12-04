b83e1f221113c8e8a9a310c3bbcdf7346d40cfc2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-04 17:39:09 +0200

    Create Comment.jsx

---
+++ client/src/components/Comment/Comment.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+
+import formatDateToTimeAgo from '../../util/formatDateToTimeAgo.js';
+import NoResults from '../NoResults/NoResults.jsx';
+
+export default function Comment({ comments }) {
+	return (
+		<>
+			{comments.map((comment) => (
+				<div
+					key={comment._id}
+					className="w-full flex flex-col gap-4 p-4 mb-8 rounded-lg bg-white"
+				>
+					<div className="flex flex-col">
+						<p className="text-xl whitespace-nowrap truncate overflow-hidden">
+							{comment.owner?.email}
+						</p>
+						<p className="text-gray-400 text-sm mb-4">
+							Добавен преди {formatDateToTimeAgo(comment._createdOn)}.
+						</p>
+					</div>
+					<p className="-mt-4 text-gray-500">{comment.comment}</p>
+				</div>
+			))}
+
+            {comments.length === 0 && <NoResults text={'Няма добавени коментари.'} textColor={'white'}/>}
+		</>
+	);
+}
