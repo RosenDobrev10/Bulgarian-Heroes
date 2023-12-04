ae9856c8cf955bce8601a5573501344cfa62fe22
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-04 18:27:54 +0200

    change switch with if else

--- client/src/components/Comment/commentReducer.js
+++ client/src/components/Comment/commentReducer.js
@@ -1,14 +1,13 @@
-export default function commentReducer(state, action){
-    switch (action?.type) {
-        case 'GET_ALL_COMMENTS_FOR_HERO':
-            return [...action.payload];
-        case 'ADD_COMMENT':
-            return [...state, action.payload];
-        case 'EDIT_COMMENT':
-            return state.map(c => c._id === action.payload._id ? { ...c, text: action.payload.text } : c);
-        case 'DELETE_COMMENT':
-            return state.filter(c => c._id !== action.payload._id)
-        default:
-            return state;
-    }
+export default function commentReducer(state, action) {
+	if (action.type === 'GET_ALL_COMMENTS_FOR_HERO') {
+		return [...action.payload];
+	} else if (action.type === 'ADD_COMMENT') {
+		return [...state, action.payload];
+	} else if (action.type === 'EDIT_COMMENT') {
+		return state.map((comment) => comment._id === action.payload._id ? { ...comment, text: action.payload.text } : comment);
+	} else if (action.type === 'DELETE_COMMENT') {
+		return state.filter((comment) => comment._id !== action.payload._id);
+	}
+
+	return state;
 }
