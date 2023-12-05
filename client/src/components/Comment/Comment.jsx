3cfe41c63a06e2e989aff23c8f570c73e6bacdfc
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-05 16:33:18 +0200

    add logic for deleting and editing own comment

--- client/src/components/Comment/Comment.jsx
+++ client/src/components/Comment/Comment.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from 'react';
 import formatDateToTimeAgo from '../../util/formatDateToTimeAgo.js';
 
 import NoResults from '../NoResults/NoResults.jsx';
+import DeleteComment from './DeleteComment/DeleteComment.jsx';
+import useAuthContext from '../../hooks/useAuthContext.js';
 
-export default function Comment({ comments }) {
+export default function Comment({ comments, deleteCommentHandler }) {
+	const [showDeleteModal, setShowDeleteModal] = useState(false);
+	const [showEditModal, setShowEditModal] = useState(false);
+	const [commentDetails, setCommentDetails] = useState({});
+	const { getUserId } = useAuthContext();
+
+	function toggleDeleteModal() {
+		setShowDeleteModal(!showDeleteModal);
+	}
+
+	function deleteClickHandler(comment){
+		toggleDeleteModal();
+		setCommentDetails(comment)
+	}
+
+	function toggleEditModal() {
+		setShowEditModal(!showEditModal);
+	}
+
 	return (
 		<>
 			{comments.map((comment) => (
@@ -13,18 +34,42 @@ export default function Comment({ comments }) {
 					className="w-full flex flex-col gap-4 p-4 mb-8 rounded-lg bg-white"
 				>
 					<div className="flex flex-col">
-						<p className="text-xl whitespace-nowrap truncate overflow-hidden">
+						<p className="text-3xl font-medium whitespace-nowrap truncate overflow-hidden">
 							{comment?.owner.email}
 						</p>
-						<p className="text-gray-400 text-sm mb-4">
+						<p className="text-gray-400 text-base mb-4">
 							Добавен преди {formatDateToTimeAgo(comment._createdOn)}.
 						</p>
 					</div>
-					<p className="-mt-4 text-gray-500">{comment.comment}</p>
+					<p className="-mt-4">{comment.comment}</p>
+					{comment._ownerId === getUserId && (
+					<div className="flex justify-between flex-wrap">
+						<div className="hover:drop-shadow-lg hover:opacity-80 bg-red-500 w-72 m-auto mt-6 p-2 rounded-2xl text-white text-center shadow-xl">
+							<button
+								className="lg:text-sm text-lg font-bold"
+								onClick={() => deleteClickHandler(comment)}
+							>
+								Изтрий
+							</button>
+						</div>
+						<div className="hover:drop-shadow-lg hover:opacity-80 bg-yellow-500 w-72 m-auto mt-6 p-2 rounded-2xl text-center text-white shadow-xl">
+							<button 
+								className="lg:text-sm text-lg font-bold"
+								onClick={toggleEditModal}
+							>
+								Промени
+							</button>
+						</div>
+					</div>
+					)}
 				</div>
 			))}
 
-            {comments.length === 0 && <NoResults text={'Няма добавени коментари.'} textColor={'white'}/>}
+			{showDeleteModal && <DeleteComment toggleDeleteModal={toggleDeleteModal} comment={commentDetails} deleteCommentHandler={deleteCommentHandler}/>}
+
+			{/* {showEditModal && <EditComment toggleEditModal={toggleEditModal} {...comment} />} */}
+
+			{comments.length === 0 && <NoResults text={'Няма добавени коментари.'} textColor={'white'}/>}
 		</>
 	);
 }
