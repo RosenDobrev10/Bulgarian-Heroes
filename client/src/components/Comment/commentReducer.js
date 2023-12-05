16501e67629fffb545e70750d9a9a49d814235c3
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-05 16:32:22 +0200

    add optional chaining to action

--- client/src/components/Comment/commentReducer.js
+++ client/src/components/Comment/commentReducer.js
@@ -1,11 +1,11 @@
 export default function commentReducer(state, action) {
 	if (action?.type === 'GET_ALL_COMMENTS_FOR_HERO') {
 		return [...action.payload];
-	} else if (action.type === 'ADD_COMMENT') {
+	} else if (action?.type === 'ADD_COMMENT') {
 		return [action.payload, ...state,];
-	} else if (action.type === 'EDIT_COMMENT') {
+	} else if (action?.type === 'EDIT_COMMENT') {
 		return state.map((comment) => comment._id === action.payload._id ? { ...comment, text: action.payload.text } : comment);
-	} else if (action.type === 'DELETE_COMMENT') {
+	} else if (action?.type === 'DELETE_COMMENT') {
 		return state.filter((comment) => comment._id !== action.payload._id);
 	}
 
