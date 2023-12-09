52db423aa5e96cf0655339cf88b5f9c80c6775a3
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-09 14:59:57 +0200

    Increase comment length to 200

--- client/src/components/Comment/commentValidation.js
+++ client/src/components/Comment/commentValidation.js
@@ -2,8 +2,8 @@ export default function commentValidation(inputName, inputValue) {
 	if (inputName === 'comment') {
 		if (inputValue === '') {
 			return { [inputName]: 'Полето е задължително !' };
-		} else if (inputValue.length > 100) {
-			return { [inputName]: 'Максимум 100 символа !' };
+		} else if (inputValue.length > 200) {
+			return { [inputName]: 'Максимум 200 символа !' };
 		}
 		return { [inputName]: '' };
     }
